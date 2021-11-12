2ca49c892a784c1d91774fd6d35320042aa861a1
Vitaliy Shulik <shulikvitalya@gmail.com>
2021-11-12 19:52:40 +0600

    add localstorage utils

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,9 +1,40 @@
+import pkg from 'src/../package.json';
+
 export const shortenText = (str = "", numberOfLetters = 7) => {
   return str?.length > numberOfLetters + 3
     ? str.substring(0, numberOfLetters).trim() + "..."
     : str;
 };
 
-export default {
+export function lsSet(key: string, value: any) {
+  return localStorage.setItem(`${pkg.name}.${key}`, JSON.stringify(value));
+}
+
+export function lsGet(key: string, fallback?: any) {
+  const item = localStorage.getItem(`${pkg.name}.${key}`);
+  return jsonParse(item, fallback);
+}
+
+export function lsRemove(key: string) {
+  return localStorage.removeItem(`${pkg.name}.${key}`);
+}
+
+export function jsonParse(input: any, fallback?: any) {
+  if (typeof input !== 'string') {
+    return fallback || {};
+  }
+  try {
+    return JSON.parse(input);
+  } catch (err) {
+    return fallback || {};
+  }
+}
+
+const defaultObj = {
   shortenText,
+  lsSet,
+  lsGet,
+  lsRemove
 };
+
+export default defaultObj;
