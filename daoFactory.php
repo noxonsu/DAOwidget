6951d60e52c6242000ed92fdbf15392b70263f97
Vitaliy Shulik <shulikvitalya@gmail.com>
2021-12-01 16:17:19 +0600

    add cargo build for wp shortcode

--- daoFactory.php
+++ daoFactory.php
@@ -6,23 +6,29 @@ Author: Vitaliy Shulik
 Requires PHP: 7.1
 Text Domain: daofactory
 Domain Path: /lang
-Version: 1.0.0
+Version: 1.0.1
  */
 
-/* Define Plugin Constants */
-defined( 'ABSPATH' ) || exit;
 
-define( 'DAOFACTORY_BASE_DIR', __DIR__ );
-define( 'DAOFACTORY_BASE_FILE', __FILE__ );
-define( 'DAOFACTORY_PATH', plugin_dir_path( __FILE__ ) );
-define( 'DAOFACTORY_URL', plugin_dir_url( __FILE__ ) );
+// First register resources with init
+function daofactory_app_init() {
+  $path = "/build/static";
 
-
-function daofactory_main_shortcode() {
-  ob_start(); ?>
-  js свой сюда вставляй
-  йа креведко <script>alert('azaza')</script>
-  <?
-	return ob_get_clean();
+  wp_register_script("daofactory_app_js", plugins_url($path."/js/main.js", __FILE__), array(), "1.0", false);
+  wp_register_style("daofactory_app_css", plugins_url($path."/css/main.css", __FILE__), array(), "1.0", "all");
 }
-add_shortcode( 'daofactoryInit', 'daofactory_main_shortcode' );
+
+add_action( 'init', 'daofactory_app_init' );
+
+// Function for the short code that call React app
+function daofactory_app() {
+  wp_enqueue_script("daofactory_app_js", '1.0', true);
+  wp_enqueue_style("daofactory_app_css");
+  /* ob_start(); ?>
+    js свой сюда вставляй
+    йа креведко <script>alert('azaza')</script>
+  <? */
+  return "<div id=\"daofactory_app\"></div>";
+}
+
+add_shortcode('daofactory_app', 'daofactory_app');
