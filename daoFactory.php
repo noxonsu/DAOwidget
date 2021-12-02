3cdc0517202c76f6b6fcec7c974da38a553bfab3
Noxon <2914674+noxonsu@users.noreply.github.com>
2021-12-02 07:14:27 +0300

    add parametrs
    сделал возможность передавать параметры в шорткод

--- daoFactory.php
+++ daoFactory.php
@@ -21,14 +21,14 @@ function daofactory_app_init() {
 add_action( 'init', 'daofactory_app_init' );
 
 // Function for the short code that call React app
-function daofactory_app() {
+function daofactory_app($atts) {
   wp_enqueue_script("daofactory_app_js", '1.0', true);
   wp_enqueue_style("daofactory_app_css");
-  /* ob_start(); ?>
-    js свой сюда вставляй
-    йа креведко <script>alert('azaza')</script>
-  <? */
-  return "<div id=\"daofactory_app\"></div>";
+  $script = "<script>";
+  if (isset($atts['ensspace'])) $script .= "window.df_ensSpace='".$atts['ensspace']."'";
+  $script .= "</script><div id=\"daofactory_app\"></div>";
+  return $script;
+  
 }
 
 add_shortcode('daofactory_app', 'daofactory_app');
