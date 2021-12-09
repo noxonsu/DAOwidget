b9e89b5cab36dd5c7d38773a8c4ef610b299df68
Vitaliy Shulik <shulikvitalya@gmail.com>
2021-12-09 13:15:12 +0600

    add create proposal title and resizable body

--- src/pages/CreateProposal/index.tsx
+++ src/pages/CreateProposal/index.tsx
@@ -1,7 +1,11 @@
+import CreateProposalBodyTextArea from "./CreateProposalBodyTextArea";
+import "./index.scss"
+
 function CreateProposal() {
   return (
-    <div>
-      <h1 style={{ textAlign: "center" }}>Create Proposal Page</h1>
+    <div className="createProposal">
+      <input maxLength={128} className="createTitleInput" placeholder="Ask a question..." />
+      <CreateProposalBodyTextArea className="createBodyTextArea" placeholder="Tell more about your proposal (optional)"/>
     </div>
   );
 }
